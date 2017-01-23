5bd47e82c3a93ec4a9ba8fad65a0f29a94945d0b
Joey Bryan <josephrbryan@gmail.com>
2017-01-22 18:39:24 -0800

    Add custom app styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,23 +1,9 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
-
-// var About = function (props) {
-//   return (
-//     <h3>About Component</h3>
-//   );
-// };
-
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Welcome to the about page!</p>
     </div>
   );
